0133c63b1edcdeee0794bebaaaedacef2effb23f
William Froass <wfroass@gmail.com>
2025-10-06 19:31:43 -0400

    graph updates

--- hardware_trojans/combinational_trojan_sm_analysis.py
+++ hardware_trojans/combinational_trojan_sm_analysis.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+
 
 
 output_dir = 'combinational_analysis_graphs'
@@ -10,12 +12,21 @@ os.makedirs(output_dir, exist_ok=True)
 df = pd.read_csv('all_stochastic_results.csv')
 
 
-# 1. Mean percent error vs. bit length (across all runs)
-mean_error = df.groupby('Number of Bits')['Percent Error (%)'].mean().reset_index()
-fig1 = px.line(mean_error, x='Number of Bits', y='Percent Error (%)', log_x=True, title='Mean Percent Error vs. Bitstream Length')
+# Mean Percent Error vs. Bitstream Length
+df['Percent Error (%)'] = pd.to_numeric(df['Percent Error (%)'], errors='coerce')
+mean_error = df.groupby('Number of Bits', as_index=False)['Percent Error (%)'].mean()
+fig1 = px.line(mean_error, x='Number of Bits', y='Percent Error (%)', log_x=True,
+             title='Mean Percent Error vs. Bitstream Length', markers=True)
 fig1.write_html(os.path.join(output_dir, 'mean_percent_error_vs_bits.html'))
 
-#2. Mean absolute error vs. bit length (across all runs)
-mean_abs_error = df.groupby('Number of Bits')['Absolute Error'].mean().reset_index()
-fig2 = px.line(mean_abs_error, x='Number of Bits', y='Absolute Error', log_x=True, title='Mean Absolute Error vs. Bitstream Length')
+
+
+# Mean Absolute Error vs. Bitstream Length
+df['Absolute Error'] = pd.to_numeric(df['Absolute Error'], errors='coerce')
+mean_abs_error = df.groupby('Number of Bits', as_index=False)['Absolute Error'].mean()
+fig2 = px.line(mean_abs_error, x='Number of Bits', y='Absolute Error',
+               log_x=True,
+               title='Mean Absolute Error vs. Bitstream Length',
+               markers=True)
+
 fig2.write_html(os.path.join(output_dir, 'mean_absolute_error_vs_bits.html'))
